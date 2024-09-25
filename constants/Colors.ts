250eba9d8a736a71df8be9cc4e293a81d8b10417
Rafael Tadashi Yokouchi <rafayokouchi@hotmail.com>
2024-09-25 13:03:31 -0300

    Commit inicial

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -6,21 +6,11 @@
 const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
 
-export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
-};
+export const colors = {
+  background: '#0f232c',
+  white: '#fff',
+  green: '#1eb500',
+  orange: '#f79600',
+  blue: '#009ad6',
+  black: '#000'
+}
---
+++ types/data.ts
@@ -0,0 +1,16 @@
+export interface Data {
+    nome: string;
+    sexo: string;
+    idade: number;
+    altura: number;
+    peso: number;
+    objetivo: number;
+    refeicoes: RefeicoesProps[];
+    suplementos: string[];
+}
+
+interface RefeicoesProps {
+    horario: string;
+    nome: string;
+    alimentos: string[];
+}
